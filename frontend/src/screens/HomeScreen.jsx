66d885f4c4b4c241c6c24d00f25c8b354736da81
Melih Dizdar <59977132+melihdizdar@users.noreply.github.com>
2022-02-14 14:10:02 +0300

    modify some changes

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -16,7 +16,7 @@ import kimyon from "../assets/kimyon.jpg";
 import nane from "../assets/nane.jpg";
 import pulbiber from "../assets/pulbiber.jpg";
 import zerdecal from "../assets/zerdecal.jpg";
-import köri from "../assets/köri.jpg";
+import kori from "../assets/kori.jpg";
 
 export default function HomeScreen() {
     return (
@@ -46,10 +46,9 @@ export default function HomeScreen() {
                 <div className="uk-container">
                     <div className="uk-margin-auto uk-flex-center uk-text-center uk-text-emphasis uk-grid-small" data-uk-grid>
                         <div className="uk-padding-remove">
-                            <h2 className="uk-width-1-1">
+                            <h2 className="uk-width-1-1 text-decoration">
                                 LEZZET EVİNİZ
                             </h2>
-                            <div className="line"/>
                         </div>
                         <p className="uk-padding-remove">
                         Yemek yapmak bir aşktır.Birbirinden lezzetli sofralar kurmak için en önem verdiğimiz şey tabi ki sevgimiz.'Lezzet Eviniz' olarak tamamen doğal ürünlerden oluşan ürünlerimizle eşsiz tatları mutfaklarınıza taşıyoruz.Ürünlerimizin malzemeleri Anadolu'da yaşayan emekçi kadınlar tarafından üretilip mutfağımızda buluşuyor.Ve bu ürünler sayesinde yöresel lezzetlerin en güzel ve en özel tariflerini sizlere sunarak,görkemli bir ziyafet deneyimi yaşamanızı garanti ediyoruz.
@@ -61,84 +60,77 @@ export default function HomeScreen() {
                 <div className="uk-container">
                     <div className="uk-margin-auto uk-flex-center uk-text-center uk-text-emphasis uk-grid-small" data-uk-grid>
                         <div className="uk-padding-remove">
-                            <h2 className="uk-width-1-1">
+                            <h2 className="uk-width-1-1 text-decoration">
                                 EL YAPIMI ÜRÜNLER
                             </h2>
-                            <div className="line"/>
                         </div>
                     </div>
                 </div>
                 <div className="uk-section uk-padding-remove-bottom">
                     <div className="uk-container">
-                        <div className="uk-grid-medium uk-margin-auto uk-child-width-1-3@m uk-child-width-1-2@s uk-flex-center" data-uk-grid>
+                        <div className="uk-grid-medium uk-child-width-1-3@m uk-child-width-1-2@s uk-grid-match" data-uk-grid>
                             <div>
                                 <div className="uk-card uk-card-default">
-                                    <div className="uk-card-media-top cs-height">
+                                    <div className="uk-card-media-top uk-height-medium">
                                         <img src={icli_kofte} alt="icli_kofte" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                     </div>
-                                    <div className="uk-card-body cs-height-body">
-                                        <h3 className="uk-card-title">İçli Köfte</h3>
-                                        <div className="line"/>
+                                    <div className="uk-card-body">
+                                        <h3 className="uk-card-title text-decoration">İçli Köfte</h3>
                                         <p>İster 5 çayının, ister akşam yemeğinin en özel vazgeçilmezi...</p>
                                     </div>
                                 </div>
                             </div>
                             <div>
                                 <div className="uk-card uk-card-default">
-                                    <div className="uk-card-media-top cs-height">
+                                    <div className="uk-card-media-top uk-height-medium">
                                         <img src={manti} alt="manti" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                     </div>
-                                    <div className="uk-card-body cs-height-body">
-                                        <h3 className="uk-card-title">Mantı</h3>
-                                        <div className="line"/>
+                                    <div className="uk-card-body">
+                                        <h3 className="uk-card-title text-decoration">Mantı</h3>
                                         <p>Sarımsaklı yoğurt ve tereyağlı sosuyla hayır diyemiyeceğiniz en güzel lezzet...</p>
                                     </div>
                                 </div>
                             </div>
                             <div>
                                 <div className="uk-card uk-card-default">
-                                    <div className="uk-card-media-top cs-height">
+                                    <div className="uk-card-media-top uk-height-medium">
                                         <img src={borek} alt="borek" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                     </div>
-                                    <div className="uk-card-body cs-height-body">
-                                        <h3 className="uk-card-title">Boşnak Böreği</h3>
-                                        <div className="line"/>
+                                    <div className="uk-card-body">
+                                        <h3 className="uk-card-title text-decoration">Boşnak Böreği</h3>
                                         <p>Kat kat açılıp, çıtır çıtır pişen efsane lezzet...</p>
                                     </div>
                                 </div>
                             </div>
                             <div>
                                 <div className="uk-card uk-card-default">
-                                    <div className="uk-card-media-top cs-height">
+                                    <div className="uk-card-media-top uk-height-medium">
                                         <img src={sarma} alt="sarma" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                     </div>
-                                    <div className="uk-card-body cs-height-body">
-                                        <h3 className="uk-card-title">Zeytinyağlı Sarma</h3>
-                                        <div className="line"/>
+                                    <div className="uk-card-body">
+                                        <h3 className="uk-card-title text-decoration">Zeytinyağlı Sarma</h3>
                                         <p>Ege'nin en güzel zeytinyağlısıyla sofranıza ayrı bir lezzet katın...</p>
                                     </div>
                                 </div>
                             </div>
                             <div>
                                 <div className="uk-card uk-card-default">
-                                    <div className="uk-card-media-top cs-height">
+                                    <div className="uk-card-media-top uk-height-medium">
                                         <img src={muhammara} alt="muhammara" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                     </div>
-                                    <div className="uk-card-body cs-height-body">
-                                        <h3 className="uk-card-title">Muhammara</h3>
-                                        <div className="line"/>
+                                    <div className="uk-card-body">
+                                        <h3 className="uk-card-title text-decoration">Muhammara</h3>
                                         <p>En tatlı acılı muhammarayı denemeye hazırmısınız?</p>
                                     </div>
                                 </div>
                             </div>
                             <div>
                                 <div className="uk-card uk-card-default">
-                                    <div className="uk-card-media-top cs-height">
+                                    <div className="uk-card-media-top uk-height-medium">
                                         <img src={cocuk_menusu} alt="cocuk_menusu" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                     </div>
-                                    <div className="uk-card-body cs-height-body">
-                                        <h3 className="uk-card-title">Çocuk Menüsü</h3>
-                                        <div className="line"/>
+                                    <div className="uk-card-body">
+                                        <h3 className="uk-card-title text-decoration">Çocuk Menüsü</h3>
                                         <p>Çocuklara özel acısız içli köfte..</p>
                                     </div>
                                 </div>
@@ -151,10 +143,9 @@ export default function HomeScreen() {
                 <div className="uk-container">
                     <div className="uk-margin-auto uk-flex-center uk-text-center uk-text-emphasis uk-grid-small" data-uk-grid>
                         <div className="uk-padding-remove">
-                            <h2 className="uk-width-1-1">
+                            <h2 className="uk-width-1-1 text-decoration">
                                 ORGANİK ÜRÜNLER
                             </h2>
-                            <div className="line"/>
                         </div>
                     </div>
                 </div>
@@ -163,40 +154,40 @@ export default function HomeScreen() {
                         <div data-uk-slider>
                             <div className="uk-position-relative">
                                 <div className="uk-slider-container uk-light">
-                                    <ul className="uk-slider-items uk-child-width-1-1 uk-child-width-1-3@s uk-child-width-1-4@m uk-flex-center uk-grid-medium" data-uk-grid>
-                                        <li className="uk-height-medium uk-width-medium">
+                                    <ul className="uk-slider-items uk-child-width-1-1 uk-child-width-1-3@s uk-child-width-1-4@m uk-grid-small" data-uk-grid>
+                                        <li>
                                             <img src={karabiber} alt="karabiber" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                         </li>
-                                        <li className="uk-height-medium uk-width-medium">
+                                        <li>
                                             <img src={kimyon} alt="kimyon" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                         </li>
-                                        <li className="uk-height-medium uk-width-medium">
+                                        <li>
                                             <img src={pulbiber} alt="pulbiber" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                         </li>
-                                        <li className="uk-height-medium uk-width-medium">
+                                        <li>
                                             <img src={zerdecal} alt="zerdecal" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                         </li>
-                                        <li className="uk-height-medium uk-width-medium">
+                                        <li>
                                             <img src={nane} alt="nane" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                         </li>
-                                        <li className="uk-height-medium uk-width-medium">
-                                            <img src={köri} alt="köri" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
+                                        <li>
+                                            <img src={kori} alt="kori" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                         </li>
-                                        <li className="uk-height-medium uk-width-medium">
+                                        <li>
                                             <img src={kekik} alt="kekik" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                         </li>
-                                        <li className="uk-height-medium uk-width-medium">
+                                        <li>
                                             <img src={biberiye} alt="biberiye" data-uk-img className="uk-width-1-1 uk-height-1-1"/>
                                         </li>
                                     </ul>
                                 </div>
-                                <div class="uk-hidden@s uk-light">
-                                    <a class="uk-position-center-left uk-position-small" href="#" data-uk-slidenav-previous data-uk-slider-item="previous"></a>
-                                    <a class="uk-position-center-right uk-position-small" href="#" data-uk-slidenav-next data-uk-slider-item="next"></a>
+                                <div className="uk-hidden@s uk-light">
+                                    <a className="uk-position-center-left uk-position-small" href="#" data-uk-slidenav-previous data-uk-slider-item="previous"></a>
+                                    <a className="uk-position-center-right uk-position-small" href="#" data-uk-slidenav-next data-uk-slider-item="next"></a>
                                 </div>
-                                <div class="uk-visible@s">
-                                    <a class="uk-position-center-left-out uk-position-small" href="#" data-uk-slidenav-previous data-uk-slider-item="previous"></a>
-                                    <a class="uk-position-center-right-out uk-position-small" href="#" data-uk-slidenav-next data-uk-slider-item="next"></a>
+                                <div className="uk-visible@s">
+                                    <a className="uk-position-center-left-out uk-position-small" href="#" data-uk-slidenav-previous data-uk-slider-item="previous"></a>
+                                    <a className="uk-position-center-right-out uk-position-small" href="#" data-uk-slidenav-next data-uk-slider-item="next"></a>
                                 </div>
                             </div>
                         </div>
